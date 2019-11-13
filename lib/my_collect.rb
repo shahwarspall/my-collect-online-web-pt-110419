37644e85244aad39e1006afd0f9888fa4f3ea93e
Shahwar Spall <shevy.spall@gmail.com>
2019-11-13 05:07:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(languages)
+  counter=0 
+  collection=[]
+  
+  while counter < languages.length 
+    collection << yield(languages[counter])
+    counter += 1
+  end 
+  collection 
+  
+end
 
